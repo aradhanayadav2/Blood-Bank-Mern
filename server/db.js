629c7b6de0ddcd7ae98bcd1153791fcb1996e310
Aradhana Yadav <yadavaradhana529@gmail.com>
2026-04-08 19:20:10 +0530

    DB fix

--- server/db.js
+++ server/db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
+// require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
+import dns from "dns";
+dns.setServers( ["8.8.8.8", "1.1.1.1"]);
 async function connectToDB(){
-    let DB = `${process.env.DB_URL}/bloodbank`
+    let DB = `${process.env.DB_URL}`
     console.log(DB)
     try{
         await mongoose.connect(DB);
